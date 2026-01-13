fa30c8c2710d06d58198cb4cc23b220db10c7384
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-01-12 21:09:28 -0500

    components start

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import components
+import FeaturedCard from './featured_card';
+
+
+function App(props) {
+    return (
+        <div>
+            <FeaturedCard />
+            <FeaturedCard />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/featured_card.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const FeaturedCard = () => {
+    return <input />;
+};
+
+
+export default FeaturedCard;

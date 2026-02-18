e54dc93e4bac783168f61a979857deb2d134ff3a
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-02-17 23:15:27 -0500

    fixing link in link

--- src/jsx/components/featuredCard.jsx
+++ src/jsx/components/featuredCard.jsx
@@ -8,14 +8,14 @@ import { ScrollReveal } from '@allwells/react-scroll-reveal';
 const FeaturedCard = (props) => {
     return (
         <ScrollReveal animationType="fade" delay={100} once={false}>
-            <Link className="featuredCardLink" to={props.link}>
                 <div className='featuredCardContainer' >
+                    <Link className="featuredCardLink" to={props.link}>
                     <img className='featuredCardImage' src={props.imgSrc}></img>
                     <h2 className='project-title font-lg'>{props.title}</h2>
                     <p className='project-blurb font-md'>{props.blurb}</p>
+                    </Link>
                     <Button link={props.link} text="Read More" />
                 </div>
-            </Link>
         </ScrollReveal>
     )
 

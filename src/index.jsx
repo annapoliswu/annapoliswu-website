5abef9cb4c763e752d5efbbb120319dbd15049a8
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-03-17 00:52:17 -0400

    Updates

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './css/style.scss';
-
-import App from './app';
-
-const root = createRoot(document.getElementById('main'));
-root.render(<App />);

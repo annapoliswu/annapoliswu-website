a1ecab9276fad9e8060a95d6ba675632c8cbf841
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-06-09 23:52:47 -0400

    veridium title update

--- src/jsx/pages/projectPages/veridium.jsx
+++ src/jsx/pages/projectPages/veridium.jsx
@@ -14,8 +14,8 @@ function Veridium(props) {
         
     return (
         <Project {...data}>
-            <h6 className="subheading">Background</h6>
-            <h2>Teaching Abstract Concepts</h2>
+            <h6 className="subheading">Overview</h6>
+            <h2>Veridium - Grounding Abstract Concepts in VR</h2>
 
             <p>Complex atomic and molecular structures are often difficult for students to comprehensively visualize, especially as 3D structures represented in traditional textbooks.</p>
             <ZoomImg imgSrc={"img/veridium-ss-diagram.jpg"} alt={"Examples of structure diagrams of various materials"}/>

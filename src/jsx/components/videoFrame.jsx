ac2ce5f82b249b7e0385e1dbf7d25f2efa8ad1a8
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-06-10 00:45:09 -0400

    video frame

---
+++ src/jsx/components/videoFrame.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../css/videoFrame.scss';
+
+const VideoFrame = (props) => {
+    return (
+        <iframe 
+            className= {'videoFrame ' + props.className} 
+            src={props.videoUrl}
+            title={props.title}
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allow="fullscreen"
+            allowFullScreen="true"
+            webkitallowfullscreen="true"
+            mozallowfullscreen="true"
+        >
+        </iframe>
+    )};
+
+export default VideoFrame;
+
--- src/jsx/pages/projectPages/soundboard.jsx
+++ src/jsx/pages/projectPages/soundboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Project from '../project';
 import ProjectData from '../../data/projectData';
+import VideoFrame from '../../components/videoFrame';
 
 function Soundboard(props) {
     let data = ProjectData.get("soundboard");
@@ -10,6 +11,10 @@ function Soundboard(props) {
             <Project {...data}>
                 <h2>Soundboard Project Page</h2>
                 <p>This is extra content inside the Soundboard project page.</p>
+                <VideoFrame
+                    videoUrl="https://www.youtube.com/embed/3UTdcA6CpkI?rel=0"
+                    title="Soundboard App Demo"
+                ></VideoFrame>
             </Project>
         </div>
     );

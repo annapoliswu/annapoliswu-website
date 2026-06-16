7b493290d4831c92dc25f2c7907fdbf3c08440a9
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-06-15 22:48:54 -0400

    left-right rail setup

--- src/jsx/pages/about.jsx
+++ src/jsx/pages/about.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import'../../css/about.scss';
 
 function About(props) {
     return (
-        <div> 
-            This is the about me.
+        <div className='aboutContainer'> 
+            <div className='leftRail'>
+                <div className='leftInfo'>
+                    <div className='photoContainer'></div>
+                    <p>This is the about me.</p>
+                </div>
+            </div>
+            <div className='rightRail'>
+                <h1 className='aboutName'>Zihan Wu</h1><h2 className='aboutTag'>UI/UX - Product Designer - Life Enjoyer</h2>
+                <p>Hi, nice to meet you! I'm Zihan, a UX / Product designer with 5+ years of experience designing elegant solutions. </p>
+            </div>
+            
         </div>
     );
 };

a38b321e1e84652cf04b5b06fbebb9dfb1ae14d3
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-06-18 01:24:12 -0400

    gallery more or less working

--- src/jsx/components/gallery.jsx
+++ src/jsx/components/gallery.jsx
@@ -1,25 +1,36 @@
 import React from "react"
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import ZoomImg from "./zoomImg"
 
 import '../../css/gallery.scss';
 
-// The number of columns and the gutter change by resizing the window
+
+import Masonry from 'react-masonry-component';
+const masonryOptions = {
+    transitionDuration: '.2s',
+    percentPosition: true,
+    columnWidth: '.gridSizer',
+    gutter: '.gutterSizer',
+    itemSelector: '.gridItem'
+};
+
+const imagesLoadedOptions = { };
+
 const Gallery = (props) => {
         return (
-            <ResponsiveMasonry className={"galleryContainer"}
-                columnsCountBreakPoints={{350: 1, 600: 2, 900: 3}}
-                gutterBreakPoints={{350: "12px", 600: "16px", 900: "24px"}}
-            >
-                <Masonry sequential={props.sequential || false}>
-                    {props.urls.map(({url, alt}, index) => {
-                        return <ZoomImg key={index + " ZoomImg"} imgSrc={url} alt={alt} ></ZoomImg>
-                    })}
-                
-                </Masonry>
-
-            </ResponsiveMasonry>
-        )
-}
+            <Masonry
+                className={'galleryGrid'} // default ''
+                elementType={'div'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                imagesLoadedOptions={imagesLoadedOptions} // default {}
+            >   <div class="gridSizer"></div>
+                <div class="gutterSizer"></div>
+                {props.urls.map(({url, alt}, index) => {
+                        return <div className="gridItem" > <ZoomImg className="gridZoomImg" key={index + " zoomImg"} imgSrc={url} alt={alt} > </ZoomImg></div>
+                })}
+            </Masonry>
+        );
+    }
 
 export default Gallery;
--- src/jsx/pages/archive.jsx
+++ src/jsx/pages/archive.jsx
@@ -9,12 +9,12 @@ function Archive(props) {
                 {url: "img/vcs-sea-2.jpg"},
                 {url: "img/vcs-sea-3.jpg"},
                 {url: "img/vcs-sea-4.jpg"},
+                {url: "img/vcs-sea-merch.jpg"},
                 {url: "img/vcs-sea-6.jpg"},
                 {url: "img/vcs-sea-7.jpg"},
-                {url: "img/vcs-sea-8.jpg"},
-                {url: "img/vcs-sea-merch.jpg"},
             ]
     //let imgs = ["img/vcs-sea-1.jpg","img/vcs-sea-2.jpg","img/vhp-market-research-features.jpg","img/vcs-sea-2.jpg","img/vhp-market-research-features.jpg","img/vhp-market-research-features.jpg"]
+    
     return (
         <div className='archiveContainer'> 
             <h1 >Gallery</h1>
@@ -23,7 +23,7 @@ function Archive(props) {
 
             <h2 id="vcs">Southeast Asian Festival</h2>
             
-            <p>Key graphics I designed for the 2026 NYC Southeast Asian Festival with over 900+ attendees. Art used for social media branding and merch.  </p>
+            <p>Key graphics I designed for the 2026 NYC Southeast Asian Festival with over 900+ attendees. Directed art for social media branding and merch.  </p>
             <Gallery urls={imgs}></Gallery>
             <h2 id="vcs-2">yo look at me</h2>
         </div>

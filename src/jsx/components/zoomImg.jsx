4147e56893c3c57c596737264c3b43df011f28cf
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2026-06-24 17:21:14 -0400

    scroll reveal imgs

--- src/jsx/components/zoomImg.jsx
+++ src/jsx/components/zoomImg.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Zoom from 'react-medium-image-zoom'
 import '../../css/reactMediumImageZoom.scss';
-
+import { ScrollReveal } from '@allwells/react-scroll-reveal';
 
 const ZoomImg = (props) => {
     return(
+    <ScrollReveal animationType="fade" delay={60} once={false} duration={500}>
     <Zoom >
         <img className={"zoomImg " + (props.className || "")} src={props.imgSrc} width={props.imgWidth} height={props.imgHeight} alt={props.alt || undefined} />
     </Zoom>
+    </ScrollReveal>
 )};
 
 export default ZoomImg;
